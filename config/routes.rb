d12b8dae1d1c50122928236a35db299c14671400
DenisePillette <denisedpillette@gmail.com>
2018-06-07 20:34:53 -0700

    updating ylep api

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,11 @@ Rails.application.routes.draw do
   resources :tables
   resources :restaurants
   resources :users
+  resources :yelp
   post 'user_token' => 'user_token#create'
- 
+
+  post 'get_restaurant' => 'yelp#get_restaurant'
+  post 'search' => 'yelp#search'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
