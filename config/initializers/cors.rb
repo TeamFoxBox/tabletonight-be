2409bfc0c94ca90ec83ba421dd72a504085ebc88
Damon Lemmons <damonlemmons@Damons-MacBook-Air.local>
2018-07-07 14:32:04 -0700

    changed cors * to front end path

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,10 +8,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
       #origins is the home page from the front end sending info
-    origins '*'
+    origins 'https://table-tonight-be.herokuapp.com'
 
 
-    resource '*',
+    resource 'https://table-tonight-be.herokuapp.com',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
