fe635f051808bc87b111f858d308729874864e57
Damon Lemmons <damonlemmons@Damons-MacBook-Air.local>
2018-07-07 15:12:32 -0700

    more cors stuff

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,5 +14,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource 'https://table-tonight-be.herokuapp.com',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      credentials: :true
   end
 end
